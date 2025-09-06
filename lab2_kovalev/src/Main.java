0002ccb898e54ded429d7df4576754bb44fac280
Keshtoim <kovalev.drey@gmail.com>
2025-09-06 07:50:03 +0300

    laboratory2_kovalev

---
+++ lab2_kovalev/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        int mul = 1;
+        for (int i = 1; i <= 5; i++) {
+            mul *= i;
+        }
+        System.out.println("Факториал числа " + number + ": " + mul);
+    }
+}
